eccf49d5bc4f4530056bc98cea281a8232ee6966
Geréb Róbert <robert@uponsmart.eu>
2026-01-11 19:12:59 +0000

    Home page emojis replaced to fa Sitemap works

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'tools',
     # 3rd party
     'allauth',
